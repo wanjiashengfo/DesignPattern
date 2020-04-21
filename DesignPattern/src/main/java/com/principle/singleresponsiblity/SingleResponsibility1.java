ecf2826c91a0611ec65c7d2f25cd0900f5088394
wanjiashengfo <493000667@qq.com>
2020-04-21 23:14:24 +0800

    second

--- DesignPattern/src/main/java/com/principle/singleresponsiblity/SingleResponsibility1.java
+++ DesignPattern/src/main/java/com/principle/singleresponsiblity/SingleResponsibility1.java
@@ -2,7 +2,15 @@ package com.principle.singleresponsiblity;
 
 public class SingleResponsibility1 {
     public static void main(String[] args) {
-
+        Vehicle vehicle = new Vehicle();
+        vehicle.run("摩托车");
+        vehicle.run("小汽车");
     }
 
+}
+
+class Vehicle{
+    public void run(String vehicle){
+        System.out.println(vehicle + "在公路上跑");
+    }
 }
