2268dd71dce17d8eac6c06ec0087701b0d10dc48
wanjiashengfo <493000667@qq.com>
2020-04-27 03:17:06 +0800

    second

--- DesignPattern/src/main/java/com/singleton/type1/Singleton1.java
+++ DesignPattern/src/main/java/com/singleton/type1/Singleton1.java
@@ -1,4 +1,21 @@
 package com.singleton.type1;
 
 public class Singleton1 {
+    public static void main(String[] args) {
+        Singleton instance = Singleton.getInstance();
+        Singleton instance1 = Singleton.getInstance();
+        System.out.println(instance == instance1);
+    }
+}
+
+class Singleton{
+    private Singleton(){
+
+    }
+
+    private final static Singleton instance = new Singleton();
+
+    public static Singleton getInstance(){
+        return instance;
+    }
 }
